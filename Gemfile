f423e3e5449daa8a5043c803555bb44a463fe98a
sdawara <siddharthdawara@gmail.com>
2013-03-08 12:19:47 +0000

    intermediate push

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@
 source "https://rubygems.org"
 
 gem "wavefile"
+gem "ffi"
+gem "ffi-portaudio"
--- script.rb
+++ script.rb
@@ -1,19 +1,39 @@
 require 'rubygems'
 require 'bundler'
 Bundler.require
+
 include WaveFile
+include FFI::PortAudio
 
-SAMPLES_PER_BUFFER = 4096
+# SAMPLES_PER_BUFFER = 4096
+# 
+# Writer.new(File.join(File.dirname(__FILE__), *%w[data newfile.wav]), Format.new(:stereo, :pcm_16, 44100)) do |writer|
+#   Reader.new(File.join(File.dirname(__FILE__), *%w[data ambience.wav])).each_buffer(SAMPLES_PER_BUFFER) do |buffer|
+#     new_buffer = Buffer.new(buffer.samples.map {|x| [x.first * -1, x.last * -1]}, buffer.send(:format))
+#     writer.write(new_buffer)
+#   end
+# end
 
-Writer.new(File.join(File.dirname(__FILE__), *%w[data newfile.wav]), Format.new(:stereo, :pcm_16, 44100)) do |writer|
-  Reader.new(File.join(File.dirname(__FILE__), *%w[data ambience.wav])).each_buffer(SAMPLES_PER_BUFFER) do |buffer|
+API.Pa_Initialize
 
-    # STDERR.puts (buffer.methods - Object.new.methods).sort
-    # STDERR.puts buffer.samples
-    # STDERR.puts buffer.class
-    # STDERR.puts buffer.samples.inspect
-      
-    new_buffer = Buffer.new(buffer.samples.map {|x| [x.first * -1, x.last * -1]}, buffer.send(:format))
-    writer.write(new_buffer)
-  end
+input = API::PaStreamParameters.new
+input[:device] = API.Pa_GetDefaultInputDevice
+input[:channelCount] = 1
+input[:sampleFormat] = API::Int16
+input[:suggestedLatency] = 0
+input[:hostApiSpecificStreamInfo] = nil
+
+output = API::PaStreamParameters.new
+output[:device] = API.Pa_GetDefaultOutputDevice
+output[:channelCount] = 1
+output[:sampleFormat] = API::Int16
+output[:suggestedLatency] = 0
+output[:hostApiSpecificStreamInfo] = nil
+
+stream = FFI::PortAudio::Stream.new
+stream.open(input, output, 44100, 1024)
+stream.start
+while 1 == 1 do
+  p "OMG"
 end
+puts output.inspect
