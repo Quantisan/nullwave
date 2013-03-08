0b6c7b959bef6d0bc8031e912e0422ad3fd13fb6
Max <max.dupenois@forward.co.uk>
2013-03-08 10:38:15 +0000

    Added timeseries version

--- script/main.R
+++ script/main.R
@@ -1,2 +1,11 @@
-require(seewave)
-require(audio)
+# library(seewave)
+# library(tuneR)
+# library(rgl)
+
+library(audio)
+
+a <- load.wave("../data/ambience.wav")
+
+write(a, file="../data/as_timeseries.rdata")
+# library(audio)
+
